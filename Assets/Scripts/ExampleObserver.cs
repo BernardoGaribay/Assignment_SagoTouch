055d3633face388e9d26c1ec346d971e8b90fb98
Bernardo Garibay <bgaribay@sagosago.com>
2022-11-08 12:31:07 -0600

    Added hit testing method with corresponding fields

--- Assets/Scripts/ExampleObserver.cs
+++ Assets/Scripts/ExampleObserver.cs
@@ -3,6 +3,29 @@ using SagoTouch;
 using Touch = SagoTouch.Touch;
 
 public class ExampleObserver : MonoBehaviour, ISingleTouchObserver {
+
+    #region Fields
+    [System.NonSerialized]
+    private Camera m_Camera;
+
+    [System.NonSerialized]
+    private Renderer m_Renderer;
+
+    [System.NonSerialized]
+    private Transform m_Transform;
+    #endregion
+
+    public Camera Camera {
+        get { return m_Camera = m_Camera ?? CameraUtils.FindRootCamera(this.Transform);}
+    }
+
+    public Renderer Renderer {
+        get { return m_Renderer = m_Renderer ?? GetComponent<Renderer>(); }
+    }
+
+    public Transform Transform {
+        get { return m_Transform = m_Transform ?? GetComponent<Transform>(); }
+    }
         
     public bool OnTouchBegan(Touch touch) {
         // ...
@@ -31,6 +54,12 @@ public class ExampleObserver : MonoBehaviour, ISingleTouchObserver {
         if(TouchDispatcher.Instance){
             TouchDispatcher.Instance.Remove(this);
         }
+    }
+
+    private bool HitTest(Touch touch) {
+        var bounds = this.Renderer.bounds;
+        bounds.extents += Vector3.forward;
+        return bounds.Contains(CameraUtils.TouchToWorldPoint(touch, this.Transform, this.Camera));
     }
 }
 
