7d0b61189f818cd2e0b47b6e4c1b399a199a812a
Bernardo Garibay <bgaribay@sagosago.com>
2022-11-08 22:03:29 -0600

    Assigned namespace to original observer

--- Assets/Scripts/ExampleObserver.cs
+++ Assets/Scripts/ExampleObserver.cs
@@ -1,94 +1,98 @@
-using UnityEngine;
-using System.Collections.Generic;
-using SagoTouch;
-using Touch = SagoTouch.Touch;
+namespace AssignmentExample {
 
-public class ExampleObserver : MonoBehaviour, ISingleTouchObserver {
+	using UnityEngine;
+	using System.Collections.Generic;
+	using SagoTouch;
+	using Touch = SagoTouch.Touch;
 
-	#region Fields
+	public class ExampleObserver : MonoBehaviour, ISingleTouchObserver {
 
-	[System.NonSerialized]
-	private Camera m_Camera;
+		#region Fields
 
-	[System.NonSerialized]
-	private Renderer m_Renderer;
+		[System.NonSerialized]
+		private Camera m_Camera;
 
-	[System.NonSerialized]
-	private List<Touch> m_Touches;
+		[System.NonSerialized]
+		private Renderer m_Renderer;
 
-	[System.NonSerialized]
-	private Transform m_Transform;
+		[System.NonSerialized]
+		private List<Touch> m_Touches;
 
-	#endregion
+		[System.NonSerialized]
+		private Transform m_Transform;
 
-
-	#region Properties
-
-	public Camera Camera {
-		get { return m_Camera = m_Camera ?? CameraUtils.FindRootCamera(this.Transform);}
-	}
-
-	public Renderer Renderer {
-		get { return m_Renderer = m_Renderer ?? GetComponent<Renderer>(); }
-	}
-
-	public Transform Transform {
-		get { return m_Transform = m_Transform ?? GetComponent<Transform>(); }
-	}
-
-	public List<Touch> Touches {
-		get { return m_Touches = m_Touches ?? new List<Touch>();}
-	}
-
-	#endregion
+		#endregion
 
 
-	#region Methods
+		#region Properties
 
-	private void OnEnable() {
-		if (TouchDispatcher.Instance){
-			TouchDispatcher.Instance.Add(this, 0, true);
+		public Camera Camera {
+			get { return m_Camera = m_Camera ?? CameraUtils.FindRootCamera(this.Transform);}
 		}
-	}
 
-	private void OnDisable() {
-		if (TouchDispatcher.Instance){
-			TouchDispatcher.Instance.Remove(this);
+		public Renderer Renderer {
+			get { return m_Renderer = m_Renderer ?? GetComponent<Renderer>(); }
 		}
-	}
 
-	private bool HitTest(Touch touch) {
-		var bounds = this.Renderer.bounds;
-		bounds.extents += Vector3.forward;
-		return bounds.Contains(CameraUtils.TouchToWorldPoint(touch, this.Transform, this.Camera));
-	}
-
-	#endregion
-
-
-	#region ISingleTouchObserver
-
-	public bool OnTouchBegan(Touch touch) {
-		if (HitTest(touch)) {
-			this.Touches.Add(touch);
-			return true;
+		public Transform Transform {
+			get { return m_Transform = m_Transform ?? GetComponent<Transform>(); }
 		}
-		return false;
-	}
 
-	public void OnTouchMoved(Touch touch) {
+		public List<Touch> Touches {
+			get { return m_Touches = m_Touches ?? new List<Touch>();}
+		}
+
+		#endregion
+
+
+		#region Methods
+
+		private void OnEnable() {
+			if (TouchDispatcher.Instance){
+				TouchDispatcher.Instance.Add(this, 0, true);
+			}
+		}
+
+		private void OnDisable() {
+			if (TouchDispatcher.Instance){
+				TouchDispatcher.Instance.Remove(this);
+			}
+		}
+
+		private bool HitTest(Touch touch) {
+			var bounds = this.Renderer.bounds;
+			bounds.extents += Vector3.forward;
+			return bounds.Contains(CameraUtils.TouchToWorldPoint(touch, this.Transform, this.Camera));
+		}
+
+		#endregion
+
+
+		#region ISingleTouchObserver
+
+		public bool OnTouchBegan(Touch touch) {
+			if (HitTest(touch)) {
+				this.Touches.Add(touch);
+				return true;
+			}
+			return false;
+		}
+
+		public void OnTouchMoved(Touch touch) {
+			
+		}
+
+		public void OnTouchEnded(Touch touch) {
+			this.Touches.Remove(touch);
+		}
+
+		//Important to always implement OnTouchCancelled, otherwise observer may look like it stopped responding to touches (not clearing m_Touch)
+		public void OnTouchCancelled(Touch touch) {
+			OnTouchEnded(touch);
+		}
+
+		#endregion
 		
 	}
-
-	public void OnTouchEnded(Touch touch) {
-		this.Touches.Remove(touch);
-	}
-
-	//Important to always implement OnTouchCancelled, otherwise observer may look like it stopped responding to touches (not clearing m_Touch)
-	public void OnTouchCancelled(Touch touch) {
-		OnTouchEnded(touch);
-	}
-
-	#endregion
 }
 
